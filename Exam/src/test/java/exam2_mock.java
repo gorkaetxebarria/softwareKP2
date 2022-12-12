03e2ead369e8579e1968f73f55c452fefc63bc52
gorka etxebarria <gorka.etxebarria@gmail.com>
2022-12-12 09:09:21 +0100

    mock branch update

--- Exam/src/test/java/exam2_mock.java
+++ Exam/src/test/java/exam2_mock.java
@@ -7,18 +7,18 @@ import static junit.framework.Assert.assertTrue;
 
 public class exam2_mock extends EasyMockSupport {
 
-    exam2 examMock;
+    exam2 ex;
 
     @Before
     public void setUp() {
-        examMock = EasyMock.partialMockBuilder(exam2.class).addMockedMethods("squareCalc", "getDigit").createMock();
+        ex = EasyMock.partialMockBuilder(exam2.class).addMockedMethods("squareCalc", "getDigit").createMock();
     }
     @Test
     public void testExam(){
-        EasyMock.expect(examMock.squareCalc(121)).andReturn(11);
-        EasyMock.expect(examMock.getDigit(11)).andReturn(1);
+        EasyMock.expect(ex.squareCalc(121)).andReturn(11);
+        EasyMock.expect(ex.getDigit(11)).andReturn(1);
         replayAll();
-        assertTrue(examMock.exam(121));
+        assertTrue(ex.exam(121));
         verifyAll();
     }
 
